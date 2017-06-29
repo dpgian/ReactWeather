14e5c877651ef9afbe64a372e2b56900883d1992
dpGian <gdexus@gmail.com>
2017-06-29 14:42:27 +0100

    Style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,29 +1,43 @@
 var React = require('react');
 var {Link} = require('react-router');
-/*
+
 var Nav = React.createClass({
+  onSearch: function (e){
+    e.preventDefault();
+    alert('Not wired up');
+  },
   render: function () {
     return (
-      <div>
-        <h2>Nav</h2>
-        <Link to='/' activeClassName='active'>Get Weather</Link>
-        <Link to='/about' activeClassName='active'>About</Link>
-        <Link to='/examples' activeClassName='active'>Examples</Link>
+      <div className='top-bar'>
+        <div className='top-bar-left'>
+          <ul className='menu'>
+            <li className='menu-text'>React Weather App</li>
+            <li>
+              <Link to='/' activeClassName='active'>Get Weather</Link>
+            </li>
+            <li>
+              <Link to='/about' activeClassName='active'>About</Link>
+            </li>
+            <li>
+              <Link to='/examples' activeClassName='active'>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className='top-bar-right'>
+          <form onSubmit={this.onSearch}>
+            <ul className='menu'>
+              <li>
+                <input type='search' placeholder='Search weather'/>
+              </li>
+              <li>
+                <input type='submit' className='button' value='Get Weather'/>
+              </li>
+            </ul>
+          </form>
+        </div>
       </div>
     );
-  }
-});
-*/
-
-var Nav = (props) => {
-  return (
-    <div>
-      <h2>Nav</h2>
-      <Link to='/' activeClassName='active'>Get Weather</Link>
-      <Link to='/about' activeClassName='active'>About</Link>
-      <Link to='/examples' activeClassName='active'>Examples</Link>
-    </div>
-  );
 }
+});
 
 module.exports = Nav;
